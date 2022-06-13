82b290289ec8d036051b575750e7bbd30493635d
vdegenne <vdegenne@gmail.com>
2022-06-13 21:55:32 +0200

    the confusion is real here

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,11 @@ import {terser} from 'rollup-plugin-terser'
 
 export default {
   input: 'src/main.ts',
-  output: { file: 'docs/app.js', format: 'esm' },
+  output: { file: 'docs/app.js', format: 'esm', sourcemap: true },
   plugins: [
-    tsc(),
+    tsc({
+      // sourceMap: true
+    }),
     resolve(),
     json(),
     process.env.minify ? terser() : {},
